1e491146e6de0a86b24e22fe3ff45c931e876d1b
Rafael Passamai <rafael.passamai@hotmail.com>
2026-02-24 20:22:15 -0300

    feat: add Fastify server setup and dotenv for environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('Hellow World!!!');
+import "dotenv/config";
+
+// Import the framework and instantiate it
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+// Declare a route
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
